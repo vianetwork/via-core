589f39e05af28d0c33e027dfe4a2236574a17748
gabrieldemian <gabrielgcr45@gmail.com>
2024-06-14 12:41:04 +0300

    readme added section: "Running our new binaries"

--- flake.nix
+++ flake.nix
@@ -227,25 +227,24 @@
           inherit hardeningEnable;
 
           shellHook = ''
-              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [pkgs.zlib]}:$LD_LIBRARY_PATH
-              export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH
-              export ZKSYNC_HOME=$PWD
-              export PATH=$ZKSYNC_HOME/bin:$PATH
-              export RUSTFLAGS='-C link-arg=-fuse-ld=${pkgs.mold}/bin/mold'
-              export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang"
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [pkgs.zlib]}:$LD_LIBRARY_PATH
+            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH
+            export ZKSYNC_HOME=$PWD
+            export PATH=$ZKSYNC_HOME/bin:$PATH
+            export RUSTFLAGS='-C link-arg=-fuse-ld=${pkgs.mold}/bin/mold'
+            export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang"
 
-              if [ "x$NIX_LD" = "x" ]; then
-                export NIX_LD="$(<${clangStdenv.cc}/nix-support/dynamic-linker)"
-              fi
-              if [ "x$NIX_LD_LIBRARY_PATH" = "x" ]; then
-                export NIX_LD_LIBRARY_PATH="$ZK_NIX_LD_LIBRARY_PATH"
-              else
-                export NIX_LD_LIBRARY_PATH="$NIX_LD_LIBRARY_PATH:$ZK_NIX_LD_LIBRARY_PATH"
-              fi
+            if [ "x$NIX_LD" = "x" ]; then
+              export NIX_LD="$(<${clangStdenv.cc}/nix-support/dynamic-linker)"
+            fi
+            if [ "x$NIX_LD_LIBRARY_PATH" = "x" ]; then
+              export NIX_LD_LIBRARY_PATH="$ZK_NIX_LD_LIBRARY_PATH"
+            else
+              export NIX_LD_LIBRARY_PATH="$NIX_LD_LIBRARY_PATH:$ZK_NIX_LD_LIBRARY_PATH"
+            fi
           '';
-            ZK_NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [ ];
-            ZK_NIX_LD = builtins.readFile "${clangStdenv.cc}/nix-support/dynamic-linker";
-          };
+          ZK_NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [];
+          ZK_NIX_LD = builtins.readFile "${clangStdenv.cc}/nix-support/dynamic-linker";
         };
       };
     });
