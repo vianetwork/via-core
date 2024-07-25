09f210a7a42257469b135a595529f2134f13857e
Hamid Bateni <hamidmrd79@gmail.com>
2024-07-25 14:42:57 +0400

    add greeting mesage

--- core/lib/via_btc_client/examples/data_inscription_example.rs
+++ core/lib/via_btc_client/examples/data_inscription_example.rs
@@ -1,68 +1,48 @@
-use clap::{App, Arg};
-use dialoguer::{theme::ColorfulTheme, Input, Select};
-use std::process::exit;
-
-#[tokio::main]
-async fn main() {
-    let matches = App::new("Interactive CLI App")
-        .version("1.0")
-        .author("Your Name <you@example.com>")
-        .about("Does awesome things interactively")
-        .arg(
-            Arg::new("config")
-                .short('c')
-                .long("config")
-                .value_name("FILE")
-                .about("Sets a custom config file")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::new("debug")
-                .short('d')
-                .long("debug")
-                .about("Turn debugging information on"),
-        )
-        .get_matches();
-
-    // You can check the values provided by arguments
-    if matches.is_present("debug") {
-        println!("Debug mode is on");
-    }
-
-    if let Some(config) = matches.value_of("config") {
-        println!("Value for config: {}", config);
-    }
-
-    // Interactive prompt using dialoguer
-    let options = vec!["Option 1", "Option 2", "Option 3"];
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Choose an option")
-        .default(0)
-        .items(&options)
-        .interact()
-        .unwrap();
-
-    println!("You selected: {}", options[selection]);
-
-    let input: String = Input::with_theme(&ColorfulTheme::default())
-        .with_prompt("Enter your name")
-        .interact_text()
-        .unwrap();
-
-    println!("Hello, {}!", input);
-
-    // Placeholder for async operation
-    if let Err(e) = async_operation().await {
-        eprintln!("Error: {}", e);
-        exit(1);
-    }
-
-    println!("Done!");
+use inquire::Text;
+use inquire::ui::{Color,  RenderConfig, Styled, StyleSheet, Attributes};
+fn main() {
+    greeting();
+    
 }
 
-async fn async_operation() -> Result<(), Box<dyn std::error::Error>> {
-    // Simulate an asynchronous operation
-    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
-    println!("Asynchronous operation completed!");
-    Ok(())
+fn greeting() {
+    let content = r#"
+    ************************************************************
+    Welcome! 
+    
+    This is an CLI application that walks you through 
+    inscribing arbitrary data into the Bitcoin testnet.
+
+    **Please before continuing make sure you have done the following:**
+    
+    1- Install electrum wallet (https://electrum.org/#download)
+    And run it in testnet mode with using the following command:
+    Linux: electrum --testnet
+    Mac: /Applications/Electrum.app/Contents/MacOS/run_electrum --testnet
+
+    2- create a p2wpkh wallet (this is the default wallet type in electrum).
+    
+    3- get some testnet coins.
+    
+    Faucet Links:
+        https://bitcoinfaucet.uo1.net/
+        https://coinfaucet.eu/en/btc-testnet/
+    
+    when you are ready, press enter to continue...
+    "#;
+
+    let mut render_config = RenderConfig::default();
+    render_config.prompt_prefix = Styled::new("$").with_fg(Color::LightRed);
+    render_config.prompt = StyleSheet::new()
+        .with_fg(Color::Grey);
+
+    let res = Text::new(content)
+        .with_render_config(render_config)
+        .prompt();
+
+    match res {
+        Ok(_) => {},
+        Err(e) => println!("Error: {}", e),
+    }
+
 }
