1a5453d6502bc153ef2f350a2047529ed0f64c36
Hamid Bateni <hamidmrd79@gmail.com>
2024-08-10 16:21:11 +0400

    adding method structure for inscriber

--- core/lib/via_btc_client/src/inscriber/mod.rs
+++ core/lib/via_btc_client/src/inscriber/mod.rs
@@ -12,6 +12,14 @@ struct Inscriber {
     context: types::InscriberContext,
 }
 
+// the upper layer call the inscriber in chainable way
+// let snapshot = inscriber_instance
+//    .inscribe(input)
+//    .await?
+//    .get_context_snapshot()
+//    .await?;
+// 
+//  persist(snapshot)
 impl Inscriber {
     pub async fn new(rpc_url: &str, network: &str, signer_private_key: &str, persisted_ctx: Option<types::InscriberContext> ) -> Result<Self> {
         let client = Box::new(BitcoinClient::new(rpc_url, network).await?);
@@ -33,19 +41,70 @@ impl Inscriber {
     pub async fn inscribe(
         &self,
         input: types::InscriberInput,
-    ) -> Result<types::InscriberOutput> {
-        self.update_context().await;
+    ) -> Result<()> {
+        self.sync_context_with_blockchain().await;
 
+        self.prepare_commit_tx_input().await;
 
+        self.prepare_commit_tx_output().await;
 
+        self.sign_commit_tx().await;
+
+        self.prepare_reveal_tx_input().await;
+
+        self.prepare_reveal_tx_output().await;
+
+        self.sign_reveal_tx().await;
+
+        self.broadcast_insription().await;
+
+        self.insert_inscription_to_context().await;
+
+        Ok(())
+    }
+
+    async fn sync_context_with_blockchain(&self) {
         todo!();
     }
 
-    async fn update_context(&self) {
+    async fn prepare_commit_tx_input(&self) {
         todo!();
     }
 
-    async fn prepare_context_for_persistence(&self) {
+    async fn prepare_commit_tx_output(&self) {
+        todo!();
+    }
+
+    async fn sign_commit_tx(&self) {
+        todo!();
+    }
+
+    async fn prepare_reveal_tx_input(&self) {
+        todo!();
+    }
+
+
+    async fn prepare_reveal_tx_output(&self) {
+        todo!();
+    }
+
+    async fn sign_reveal_tx(&self) {
+        todo!();
+    }
+
+    async fn broadcast_insription(&self) {
+        todo!();
+    }
+
+    async fn insert_inscription_to_context(&self) {
+        todo!();
+    }
+
+    pub async fn get_context_snapshot(&self) {
+        todo!();
+    }
+
+    pub async fn recreate_context_from_snapshot() {
         todo!();
     }
 
