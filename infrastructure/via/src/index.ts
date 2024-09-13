85dae4e3a3603e62591ebf903010de5dd5c4fb73
Steph Sinyakov <steph.sinyakov@gmail.com>
2024-09-13 18:26:18 +0200

    feat: add basic commands for new cli `via`

---
+++ infrastructure/via/src/index.ts
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+import { program, Command } from 'commander';
+import { spawnSync } from 'child_process';
+import { serverCommand as server} from './server';
+import { command as up } from './up';
+import { command as down } from './down';
+import { command as completion } from './completion';
+import { initCommand } from './init';
+import { command as run } from './run';
+import { command as docker } from './docker';
+import { command as config } from './config';
+import { command as clean } from './clean';
+import { command as db } from './database';
+import * as env from './env';
+
+const COMMANDS = [
+    server,
+    up,
+    down,
+    db,
+    initCommand,
+    run,
+    docker,
+    config,
+    clean,
+    env.command,
+    completion(program as Command)
+];
+
+async function main() {
+    const cwd = process.cwd();
+    const VIA_HOME = process.env.VIA_HOME;
+
+    if (!VIA_HOME) {
+        throw new Error('Please set $VIA_HOME to the root of Via repo!');
+    } else {
+        process.chdir(VIA_HOME);
+    }
+
+    env.load();
+
+    program.version('0.1.0').name('via').description('via workflow tools');
+
+    for (const command of COMMANDS) {
+        program.addCommand(command);
+    }
+
+    // f command is special-cased because it is necessary
+    // for it to run from $PWD and not from $VIA_HOME
+    program
+        .command('f <command...>')
+        .allowUnknownOption()
+        .action((command: string[]) => {
+            process.chdir(cwd);
+            const result = spawnSync(command[0], command.slice(1), { stdio: 'inherit' });
+            if (result.error) {
+                throw result.error;
+            }
+            process.exitCode = result.status || undefined;
+        });
+
+    await program.parseAsync(process.argv);
+}
+
+main().catch((err: Error) => {
+    console.error('Error:', err.message || err);
+    process.exitCode = 1;
+});
