b66c467f6aa7b24965c51879b9826dc61bee4111
Tomasz Grześkiewicz <lemures64@gmail.com>
2024-09-26 09:57:22 +0200

    feat(eth-watch): redesign to support multiple chains (#2867)
    This PR contains code from sync-layer-stable + I removed old upgrades
    processor and updated unit-tests to use the new one
    
    ---------
    
    Signed-off-by: tomg10 <lemures64@gmail.com>

--- core/lib/contracts/src/lib.rs
+++ core/lib/contracts/src/lib.rs
@@ -48,9 +48,14 @@ const DIAMOND_INIT_CONTRACT_FILE: (&str, &str) = (
 );
 const GOVERNANCE_CONTRACT_FILE: (&str, &str) = ("governance", "IGovernance.sol/IGovernance.json");
 const CHAIN_ADMIN_CONTRACT_FILE: (&str, &str) = ("governance", "IChainAdmin.sol/IChainAdmin.json");
+const GETTERS_FACET_CONTRACT_FILE: (&str, &str) = (
+    "state-transition/chain-interfaces",
+    "IGetters.sol/IGetters.json",
+);
 
 const MULTICALL3_CONTRACT_FILE: (&str, &str) = ("dev-contracts", "Multicall3.sol/Multicall3.json");
 const VERIFIER_CONTRACT_FILE: (&str, &str) = ("state-transition", "Verifier.sol/Verifier.json");
+
 const _IERC20_CONTRACT_FILE: &str =
     "contracts/l1-contracts/artifacts/contracts/common/interfaces/IERC20.sol/IERC20.json";
 const _FAIL_ON_RECEIVE_CONTRACT_FILE:  &str  =
@@ -132,6 +137,10 @@ pub fn governance_contract() -> Contract {
 
 pub fn chain_admin_contract() -> Contract {
     load_contract_for_both_compilers(CHAIN_ADMIN_CONTRACT_FILE)
+}
+
+pub fn getters_facet_contract() -> Contract {
+    load_contract_for_both_compilers(GETTERS_FACET_CONTRACT_FILE)
 }
 
 pub fn state_transition_manager_contract() -> Contract {
