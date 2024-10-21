ab076e016e3691a4738fce8c6e09861441a1c9cc
Yury Akudovich <ya@matterlabs.dev>
2024-10-21 16:01:59 +0200

    feat(prover): Add min_provers and dry_run features. Improve metrics and test. (#3129)
    Improve metrics and test.
    Add min_provers config.
    Add dry_run config option for Agent.
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [x] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [x] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [x] Code has been formatted via `zkstack dev fmt` and `zkstack dev
    lint`.

--- prover/crates/bin/prover_autoscaler/src/global/watcher.rs
+++ prover/crates/bin/prover_autoscaler/src/global/watcher.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, sync::Arc};
 
-use anyhow::{Context, Ok};
+use anyhow::{anyhow, Context, Ok, Result};
 use futures::future;
 use reqwest::Method;
 use tokio::sync::Mutex;
@@ -9,18 +9,37 @@ use zksync_utils::http_with_retries::send_request_with_retries;
 
 use crate::{
     cluster_types::{Cluster, Clusters},
+    metrics::{AUTOSCALER_METRICS, DEFAULT_ERROR_CODE},
     task_wiring::Task,
 };
 
-#[derive(Clone)]
+const MAX_RETRIES: usize = 5;
+
+#[derive(Default)]
+pub struct WatchedData {
+    pub clusters: Clusters,
+    pub is_ready: Vec<bool>,
+}
+
+pub fn check_is_ready(v: &Vec<bool>) -> Result<()> {
+    for b in v {
+        if !b {
+            return Err(anyhow!("Clusters data is not ready"));
+        }
+    }
+    Ok(())
+}
+
+#[derive(Default, Clone)]
 pub struct Watcher {
     /// List of base URLs of all agents.
     pub cluster_agents: Vec<Arc<Url>>,
-    pub clusters: Arc<Mutex<Clusters>>,
+    pub data: Arc<Mutex<WatchedData>>,
 }
 
 impl Watcher {
     pub fn new(agent_urls: Vec<String>) -> Self {
+        let size = agent_urls.len();
         Self {
             cluster_agents: agent_urls
                 .into_iter()
@@ -31,8 +50,11 @@ impl Watcher {
                     )
                 })
                 .collect(),
-            clusters: Arc::new(Mutex::new(Clusters {
-                clusters: HashMap::new(),
+            data: Arc::new(Mutex::new(WatchedData {
+                clusters: Clusters {
+                    clusters: HashMap::new(),
+                },
+                is_ready: vec![false; size],
             })),
         }
     }
@@ -45,7 +67,8 @@ impl Task for Watcher {
             .cluster_agents
             .clone()
             .into_iter()
-            .map(|a| {
+            .enumerate()
+            .map(|(i, a)| {
                 tracing::debug!("Getting cluster data from agent {}.", a);
                 tokio::spawn(async move {
                     let url: String = a
@@ -54,14 +77,19 @@ impl Task for Watcher {
                         .context("Failed to join URL with /cluster")?
                         .to_string();
                     let response =
-                        send_request_with_retries(&url, 5, Method::GET, None, None).await;
-                    response
-                        .map_err(|err| {
-                            anyhow::anyhow!("Failed fetching cluster from url: {url}: {err:?}")
-                        })?
+                        send_request_with_retries(&url, MAX_RETRIES, Method::GET, None, None).await;
+
+                    let response = response.map_err(|err| {
+                        // TODO: refactor send_request_with_retries to return status.
+                        AUTOSCALER_METRICS.calls[&(url.clone(), DEFAULT_ERROR_CODE)].inc();
+                        anyhow::anyhow!("Failed fetching cluster from url: {url}: {err:?}")
+                    })?;
+                    AUTOSCALER_METRICS.calls[&(url, response.status().as_u16())].inc();
+                    let response = response
                         .json::<Cluster>()
                         .await
-                        .context("Failed to read response as json")
+                        .context("Failed to read response as json");
+                    Ok((i, response))
                 })
             })
             .collect();
@@ -71,18 +99,16 @@ impl Task for Watcher {
                 .await
                 .into_iter()
                 .map(|h| async move {
-                    let c = h.unwrap().unwrap();
-                    self.clusters
-                        .lock()
-                        .await
-                        .clusters
-                        .insert(c.name.clone(), c);
+                    let (i, res) = h??;
+                    let c = res?;
+                    let mut guard = self.data.lock().await;
+                    guard.clusters.clusters.insert(c.name.clone(), c);
+                    guard.is_ready[i] = true;
                     Ok(())
                 })
                 .collect::<Vec<_>>(),
         )
-        .await
-        .unwrap();
+        .await?;
 
         Ok(())
     }
