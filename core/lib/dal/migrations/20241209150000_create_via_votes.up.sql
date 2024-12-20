d0af85640bc09f0c4fdd120fe27eb31b6de3872c
0xatomFusion <179967211+0xatomFusion@users.noreply.github.com>
2024-12-20 17:33:30 +0100

    feat(dal): add dal helpers for votable transactions

--- core/lib/dal/migrations/20241209150000_create_via_votes.up.sql
+++ core/lib/dal/migrations/20241209150000_create_via_votes.up.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS via_votable_transactions (
     l1_batch_number BIGINT NOT NULL REFERENCES l1_batches (number) ON DELETE CASCADE,
     tx_id BYTEA,
     is_finalized BOOLEAN NOT NULL DEFAULT FALSE,
+    is_verified BOOLEAN NOT NULL DEFAULT FALSE,
+    l1_batch_status BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (l1_batch_number, tx_id)
