6d32d5bc982212951f169ac187cb8aa19c7fb9e1
0xatomFusion <179967211+0xatomFusion@users.noreply.github.com>
2024-12-27 09:24:18 +0100

    feat(config): add via verifier config

--- core/lib/config/src/configs/via_verifier.rs
+++ core/lib/config/src/configs/via_verifier.rs
@@ -1,21 +1,56 @@
-use std::time::Duration;
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    time::Duration,
+};
 
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub enum VerifierRole {
+    VERIFIER = 0,
+    COORDINATOR = 1,
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct ViaVerifierConfig {
-    /// How often the verifier should run its checks (milliseconds).
+    /// Interval between polling db for verification requests (in ms).
     pub poll_interval: u64,
+    /// Coordinator server port.
+    pub port: u16,
+    /// Coordinator server url.
+    pub url: String,
+    /// The signer private key.
+    pub private_key: String,
+    /// The verifiers public keys.
+    pub verifiers_pub_keys_str: Vec<String>,
+    /// The bridge address.
+    pub bridge_address_str: String,
+    /// The minimum required signers.
+    pub required_signers: usize,
+    /// The role.
+    pub role: VerifierRole,
 }
 
 impl ViaVerifierConfig {
-    pub fn poll_interval(&self) -> Duration {
+    pub fn polling_interval(&self) -> Duration {
         Duration::from_millis(self.poll_interval)
     }
+    pub fn bind_addr(&self) -> SocketAddr {
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), self.port)
+    }
+}
 
+impl ViaVerifierConfig {
     pub fn for_tests() -> Self {
         Self {
+            private_key: "private".to_string(),
             poll_interval: 1000,
+            port: 0,
+            url: "".to_string(),
+            verifiers_pub_keys_str: Vec::new(),
+            bridge_address_str: "".to_string(),
+            required_signers: 2,
+            role: VerifierRole::VERIFIER,
         }
     }
 }
