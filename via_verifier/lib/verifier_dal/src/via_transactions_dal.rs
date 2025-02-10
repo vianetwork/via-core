e0a3dc333d9561db1eee0c9e9c805fa4a1f7c28a
0xatomFusion <179967211+0xatomFusion@users.noreply.github.com>
2025-02-10 08:23:03 +0100

    feat: create a new transactions dal to track the users deposits from l1 to l2

---
+++ via_verifier/lib/verifier_dal/src/via_transactions_dal.rs
@@ -0,0 +1,126 @@
+use zksync_db_connection::{connection::Connection, error::DalResult, instrument::InstrumentExt};
+use zksync_types::H256;
+
+use crate::Verifier;
+
+#[derive(Debug)]
+pub struct ViaTransactionsDal<'a, 'c> {
+    pub(crate) storage: &'a mut Connection<'c, Verifier>,
+}
+
+impl ViaTransactionsDal<'_, '_> {
+    pub async fn insert_transaction(
+        &mut self,
+        priority_id: i64,
+        tx_id: H256,
+        receiver: String,
+        value: i64,
+        calldata: Vec<u8>,
+        canonical_tx_hash: H256,
+    ) -> DalResult<()> {
+        sqlx::query!(
+            r#"
+            INSERT INTO
+                via_transactions (priority_id, tx_id, receiver, value, calldata, canonical_tx_hash)
+            VALUES
+                ($1, $2, $3, $4, $5, $6)
+            ON CONFLICT (tx_id) DO NOTHING
+            "#,
+            priority_id,
+            tx_id.as_bytes(),
+            receiver,
+            value,
+            calldata,
+            canonical_tx_hash.as_bytes(),
+        )
+        .instrument("insert_transaction")
+        .fetch_optional(self.storage)
+        .await?;
+
+        Ok(())
+    }
+
+    pub async fn get_last_priority_id(&mut self) -> DalResult<i64> {
+        let priority_id = sqlx::query_scalar!(
+            r#"
+            SELECT COUNT(priority_id) as priority_id FROM via_transactions;
+            "#
+        )
+        .instrument("get_last_priority_id")
+        .fetch_one(self.storage)
+        .await?;
+
+        Ok(priority_id.unwrap_or(0))
+    }
+
+    pub async fn list_transactions_not_processed(&mut self, limit: i64) -> DalResult<Vec<Vec<u8>>> {
+        let rows = sqlx::query!(
+            r#"
+            SELECT
+                canonical_tx_hash
+            FROM
+                via_transactions
+            WHERE
+                status IS NULL
+            ORDER BY
+                priority_id ASC
+            LIMIT
+                $1
+            "#,
+            limit
+        )
+        .instrument("list_transactions")
+        .fetch_all(self.storage)
+        .await?;
+
+        let mut canonical_tx_hashs: Vec<Vec<u8>> = Vec::with_capacity(rows.len());
+        for row in rows {
+            canonical_tx_hashs.push(row.canonical_tx_hash);
+        }
+        Ok(canonical_tx_hashs)
+    }
+
+    pub async fn update_transaction(
+        &mut self,
+        canonical_tx_hash: &H256,
+        status: bool,
+    ) -> DalResult<()> {
+        sqlx::query!(
+            r#"
+            UPDATE via_transactions
+            SET
+                status = $2
+            WHERE
+                canonical_tx_hash = $1
+            "#,
+            canonical_tx_hash.as_bytes(),
+            status
+        )
+        .instrument("update_transaction")
+        .fetch_optional(self.storage)
+        .await?;
+
+        Ok(())
+    }
+
+    pub async fn transaction_exists_with_txid(&mut self, tx_id: &H256) -> DalResult<bool> {
+        let exists = sqlx::query!(
+            r#"
+            SELECT
+                1 AS cnt
+            FROM
+                via_transactions
+            WHERE
+                tx_id = $1
+            LIMIT
+                1
+            "#,
+            tx_id.as_bytes(),
+        )
+        .instrument("transaction_exists_with_txid")
+        .fetch_optional(self.storage)
+        .await?;
+
+        Ok(exists.is_some())
+    }
+}
