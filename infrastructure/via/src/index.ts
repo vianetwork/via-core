eccddd2c99c98e8feb0668f43cb1f0e91a67a72c
0xatomFusion <179967211+0xatomFusion@users.noreply.github.com>
2025-06-10 11:00:13 +0100

    feat(l1_indexer): add the l1 indexer to the via cli and makefile

--- infrastructure/via/src/index.ts
+++ infrastructure/via/src/index.ts
@@ -21,6 +21,7 @@ import { command as btc_explorer } from './btc_explorer';
 import { command as token } from './token';
 import { command as contract } from './contract';
 import { command as test } from './test/test';
+import { indexerCommand as indexer } from './indexer';
 
 const COMMANDS = [
     server,
@@ -41,6 +42,7 @@ const COMMANDS = [
     token,
     contract,
     test,
+    indexer,
     completion(program as Command)
 ];
 
