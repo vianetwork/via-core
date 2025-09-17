5f97c9944a1212e8961469d1009d39075e9b7388
0xatomFusion <179967211+0xatomFusion@users.noreply.github.com>
2025-09-17 17:19:49 +0100

    feat(via_verifier_btc_watch): moving the state init from the verifier btc watch to verifier init module

--- via_verifier/node/via_verifier_storage_init/src/lib.rs
+++ via_verifier/node/via_verifier_storage_init/src/lib.rs
@@ -1,96 +1,41 @@
 mod genesis;
-mod wallets;
+mod indexer;
+pub mod wallets;
 
-use std::{future::Future, sync::Arc, time::Duration};
+use std::sync::Arc;
 
 use genesis::VerifierGenesis;
-use tokio::sync::watch;
-use via_btc_client::client::BitcoinClient;
+use via_btc_client::{bootstrap::ViaBootstrap, client::BitcoinClient};
 use via_verifier_dal::{ConnectionPool, Verifier};
 use wallets::ViaWalletsInitializer;
-use zksync_config::{configs::via_consensus::ViaGenesisConfig, GenesisConfig};
-use zksync_node_storage_init::InitializeStorage;
-use zksync_types::via_wallet::SystemWallets;
+use zksync_config::{configs::via_consensus::ViaGenesisConfig, GenesisConfig, ViaBtcWatchConfig};
+
+use crate::indexer::ViaIndexerInitializer;
 
 #[derive(Debug, Clone)]
-pub struct ViaVerifierStorageInitializer {
-    genesis: Arc<dyn InitializeStorage>,
-    wallets: ViaWalletsInitializer,
-}
+pub struct ViaVerifierStorageInitializer {}
 
 impl ViaVerifierStorageInitializer {
-    pub fn new(
-        via_genesis_config: ViaGenesisConfig,
-        genesis_config: GenesisConfig,
+    pub async fn new(
         pool: ConnectionPool<Verifier>,
         client: Arc<BitcoinClient>,
-    ) -> Self {
+        via_genesis_config: ViaGenesisConfig,
+        genesis_config: GenesisConfig,
+        btc_watch_config: ViaBtcWatchConfig,
+    ) -> anyhow::Result<Self> {
         let genesis = Arc::new(VerifierGenesis {
             genesis_config,
             pool: pool.clone(),
         });
-        let wallets = ViaWalletsInitializer::new(pool, client, via_genesis_config);
-        Self { genesis, wallets }
+
+        let bootstrap = ViaBootstrap::new(client, via_genesis_config);
+        let indexer = ViaIndexerInitializer::new(pool.clone(), bootstrap.clone(), btc_watch_config);
+        let wallets = ViaWalletsInitializer::new(pool, bootstrap);
+
+        genesis.initialize_storage().await?;
+        wallets.initialize_storage().await?;
+        indexer.initialize_storage().await?;
+
+        Ok(Self {})
     }
-
-    pub async fn indexer_wallets(&self) -> anyhow::Result<SystemWallets> {
-        if let Some(system_wallets) = self.wallets.fetch_indexer_wallets_from_db().await? {
-            return Ok(system_wallets);
-        }
-        Ok(self.wallets.init_indexer_wallets().await?)
-    }
-
-    pub async fn run(self, stop_receiver: watch::Receiver<bool>) -> anyhow::Result<()> {
-        self.genesis
-            .initialize_storage(stop_receiver.clone())
-            .await?;
-
-        Ok(())
-    }
-
-    /// Checks if the node can safely start operating.
-    pub async fn wait_for_initialized_storage(
-        &self,
-        stop_receiver: watch::Receiver<bool>,
-    ) -> anyhow::Result<()> {
-        const POLLING_INTERVAL: Duration = Duration::from_secs(1);
-
-        // Wait until data is added to the database.
-        poll(stop_receiver.clone(), POLLING_INTERVAL, || {
-            self.is_database_initialized()
-        })
-        .await?;
-        if *stop_receiver.borrow() {
-            return Ok(());
-        }
-
-        Ok(())
-    }
-
-    async fn is_database_initialized(&self) -> anyhow::Result<bool> {
-        // We're fine if the database is initialized in any meaningful way we can check.
-        if self.genesis.is_initialized().await? {
-            return Ok(true);
-        }
-        Ok(false)
-    }
-}
-
-async fn poll<F, Fut>(
-    mut stop_receiver: watch::Receiver<bool>,
-    polling_interval: Duration,
-    mut check: F,
-) -> anyhow::Result<()>
-where
-    F: FnMut() -> Fut,
-    Fut: Future<Output = anyhow::Result<bool>>,
-{
-    while !*stop_receiver.borrow() && !check().await? {
-        // Return value will be checked on the next iteration.
-        tokio::time::timeout(polling_interval, stop_receiver.changed())
-            .await
-            .ok();
-    }
-
-    Ok(())
 }
--- via_verifier/node/via_verifier_storage_init/src/wallets.rs
+++ via_verifier/node/via_verifier_storage_init/src/wallets.rs
@@ -1,8 +1,5 @@
-use std::sync::Arc;
-
-use via_btc_client::{bootstrap::ViaBootstrap, client::BitcoinClient};
+use via_btc_client::bootstrap::ViaBootstrap;
 use via_verifier_dal::{ConnectionPool, Verifier, VerifierDal};
-use zksync_config::configs::via_consensus::ViaGenesisConfig;
 use zksync_types::via_wallet::{SystemWallets, SystemWalletsDetails};
 
 #[derive(Debug, Clone)]
@@ -12,49 +9,30 @@ pub struct ViaWalletsInitializer {
 }
 
 impl ViaWalletsInitializer {
-    pub fn new(
-        pool: ConnectionPool<Verifier>,
-        client: Arc<BitcoinClient>,
-        config: ViaGenesisConfig,
-    ) -> Self {
-        let bootstrap = ViaBootstrap::new(client, config);
-
+    pub fn new(pool: ConnectionPool<Verifier>, bootstrap: ViaBootstrap) -> Self {
         Self { pool, bootstrap }
     }
 
-    pub(crate) async fn init_indexer_wallets(&self) -> anyhow::Result<SystemWallets> {
-        let state = self.bootstrap.process_bootstrap_messages().await?;
-
-        let indexer_wallets_details = SystemWalletsDetails::try_from(&state)?;
-
-        self.pool
-            .connection()
-            .await?
-            .via_wallet_dal()
-            .insert_wallets(&indexer_wallets_details)
-            .await?;
-
-        let wallets = state
-            .wallets
-            .clone()
-            .ok_or_else(|| anyhow::anyhow!("Wallets missing when init_indexer_wallets"))?;
-
-        tracing::info!("Loaded the indexer wallets from bootstrap inscriptions");
-
-        Ok(wallets)
+    pub async fn load_system_wallets(
+        pool: ConnectionPool<Verifier>,
+    ) -> anyhow::Result<SystemWallets> {
+        if let Some(system_wallet) =
+            ViaWalletsInitializer::fetch_indexer_wallets_from_db(&pool).await?
+        {
+            return Ok(system_wallet);
+        }
+        anyhow::bail!("System wallets not initialized")
     }
 
-    pub(crate) async fn fetch_indexer_wallets_from_db(
-        &self,
+    pub async fn fetch_indexer_wallets_from_db(
+        pool: &ConnectionPool<Verifier>,
     ) -> anyhow::Result<Option<SystemWallets>> {
-        let system_wallet_raw_opt = self
-            .pool
+        let system_wallet_raw_opt = pool
             .connection()
             .await?
             .via_wallet_dal()
             .get_system_wallets_raw()
             .await?;
-
         let Some(system_wallet_raw) = system_wallet_raw_opt else {
             return Ok(None);
         };
@@ -63,5 +41,33 @@ impl ViaWalletsInitializer {
         tracing::info!("Loaded the indexer wallets from DB");
 
         Ok(Some(parsed_system_wallets))
+    }
+}
+
+impl ViaWalletsInitializer {
+    pub async fn is_initialized(&self) -> anyhow::Result<bool> {
+        Ok(
+            ViaWalletsInitializer::fetch_indexer_wallets_from_db(&self.pool)
+                .await?
+                .is_some(),
+        )
+    }
+
+    pub async fn initialize_storage(&self) -> anyhow::Result<()> {
+        if !self.is_initialized().await? {
+            let state = self.bootstrap.process_bootstrap_messages().await?;
+
+            let indexer_wallets_details = SystemWalletsDetails::try_from(&state)?;
+
+            self.pool
+                .connection()
+                .await?
+                .via_wallet_dal()
+                .insert_wallets(&indexer_wallets_details)
+                .await?;
+
+            tracing::info!("System wallets initialized");
+        }
+        Ok(())
     }
 }
